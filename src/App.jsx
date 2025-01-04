0eb8b046b7d88107e73427df29693d469ae48e5c
BoMaassen <bomaassen@live.nl>
2025-01-04 14:00:34 +0100

    opzet nav af

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 
 import './App.css'
+import {MagnifyingGlass, PlusCircle, UserCircle} from "@phosphor-icons/react";
 
 function App() {
 
   return (
     <>
-      <h1>Pattern Point</h1>
+<nav>
+  <h2>Pattern point</h2>
+    <div>
+  <label htmlFor="searchbar">
+  <input id="searchbar" name="searchbar" placeholder="zoeken..."/>
+  </label>
+    <button type="button"><MagnifyingGlass></MagnifyingGlass></button>
+    </div>
+  <div>
+      <button>
+          <PlusCircle></PlusCircle>
+      </button>
+      <button>
+          <UserCircle></UserCircle>
+      </button>
+  </div>
+</nav>
         <h2>titel 2</h2>
         <h3>titel 3</h3>
         <h4>titel 4</h4>
