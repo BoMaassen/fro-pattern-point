0364375b754aa85bde18d0d497fda95c4ef44c58
BoMaassen <bomaassen@live.nl>
2025-01-10 13:39:07 +0100

    formulier aangemaakt

--- src/pages/new-post/NewPost.jsx
+++ src/pages/new-post/NewPost.jsx
@@ -1,6 +1,33 @@
 function NewPost() {
     return <main>
-<h1>New post</h1>
+        <h1>New post</h1>
+        <section>
+            <form>
+                <label htmlFor="content-img">
+                    Upload een foto
+                </label>
+                <input id="content-img" name="content-img" type="file" accept="image/png, image/jpeg"/>
+                <label htmlFor="title">
+                    Titel
+                </label>
+                <input id="title" name="title" type="text"/>
+                <label htmlFor="category">
+                    Categorie
+                </label>
+                <select id="category" name="category">
+                    <option value="Truien">Truien</option>
+                    <option value="Broeken">Broeken</option>
+                    <option value="Mutsen">Mutsen</option>
+                    <option value="Sjaals">Sjaals</option>
+                    <option value="Tassen">Tassen</option>
+                    <option value="Knuffels">Knuffels</option>
+                </select>
+                <label htmlFor="description">
+                    Beschrijving
+                </label>
+                <textarea id="description" name="description"></textarea>
+            </form>
+        </section>
     </main>
 }
 
