c58dd87b498ff5c6c9f101c0b30b89644f697d17
BoMaassen <bomaassen@live.nl>
2025-01-13 17:10:09 +0100

    overview af met masonry layout

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,12 +1,37 @@
 import "./Account.css";
+import {posts} from "../../constance/posts.js";
+import Post from "../../components/post/Post.jsx";
+import {useEffect} from "react";
+import imagesLoaded from "imagesloaded";
+import Masonry from "masonry-layout";
 
 function Account(){
+
+    useEffect(() => {
+        let masonryInstance;
+        imagesLoaded('.overview', () => {
+            masonryInstance = new Masonry('.overview', {
+                itemSelector: ".post-small",
+                columnWidth: ".post-small",
+                gutter: 15,
+            });
+        });
+
+        return () => {
+            if (masonryInstance) {
+                masonryInstance.destroy();
+            }
+        };
+    }, []);
     return ( <main>
 <h1>Account</h1>
             <section className="account-container">
                 <div className="post-overview" >
-                    <div><h2>placeholder voor buttons</h2></div>
-                    <div><h2>placeholder voor posts</h2></div>
+                    <div className="maps"><h2>placeholder voor buttons</h2></div>
+                    <div className="overview"> {posts.map((post)=> (
+                            <Post className="post-small" title={post.title} img={post.imageSrc} alt={post.altText} profilePiture={post.userIcon} username={post.username} key={post.id}/>
+                        )
+                    )}</div>
                 </div>
 
                 <div>
