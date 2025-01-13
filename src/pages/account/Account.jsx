85d1fd2cfb85f3b5c461994ea5d2e32bd26d65b7
BoMaassen <bomaassen@live.nl>
2025-01-13 15:49:24 +0100

    opzet account pagina

---
+++ src/pages/account/Account.jsx
@@ -0,0 +1,24 @@
+import "./Account.css";
+
+function Account(){
+    return ( <main>
+<h1>Account</h1>
+            <section className="account-container">
+                <div className="post-overview" >
+                    <div><h2>placeholder voor buttons</h2></div>
+                    <div><h2>placeholder voor posts</h2></div>
+                </div>
+
+                <div>
+                <div className="user-card"><h2>placeholder voor user card</h2></div>  {/*user card*/}
+                <div className="notifications"><h2>placeholder voor meldingen</h2></div> {/*meldingen*/}
+                </div>
+
+            </section>
+
+
+        </main>
+
+    )
+}
+export default Account;
