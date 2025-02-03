2d826cef8f5d489b383f8a9367b647a00b5be197
BoMaassen <bomaassen@live.nl>
2025-02-03 20:31:48 +0100

    comment component gemaakt

--- src/pages/post-details/PostDetails.jsx
+++ src/pages/post-details/PostDetails.jsx
@@ -1,5 +1,5 @@
 import "./PostDetails.css"
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import Post from "../../components/post/Post.jsx";
@@ -14,11 +14,13 @@ import closeIcon from "../../assets/icons/close icon.svg";
 import arrowDownBlue from "../../assets/icons/arrow down-blue.svg"
 import arrowDownWhite from "../../assets/icons/arrow down.svg"
 import heartOutline from "../../assets/icons/Heart-outline.svg"
+import Comment from "../../components/comment/Comment.jsx";
 
 function PostDetails() {
     const [postDetail, setPostDetail] = useState({});
     const [patterns, setPatterns] = useState([]);
     const [comments, setComments] = useState([]);
+    const navigate = useNavigate();
     const {id} = useParams();
     const {register, handleSubmit, formState: {errors}} = useForm();
 
@@ -35,13 +37,12 @@ function PostDetails() {
             });
             console.log(result.data.id);
         } catch (e) {
-            console.log("er ging wat fout " + e);
+            console.error("er ging wat fout " + e);
         }
     }
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-
         async function fetchPost() {
             try {
                 const result = await axios.get(`http://localhost:8080/posts/${id}`,
@@ -53,10 +54,9 @@ function PostDetails() {
                     })
                 setPostDetail(result.data);
             } catch (e) {
-                console.log("Er ging iets mis met het ophalen van de posts probeer het opniew! " + e)
+                console.error("Er ging iets mis met het ophalen van de posts probeer het opniew! " + e)
             }
         }
-
         fetchPost();
 
         async function fetchPatterns() {
@@ -71,42 +71,37 @@ function PostDetails() {
                 setPatterns(result.data);
                 console.log(result.data);
             } catch (e) {
-                console.log("Er ging iets mis met het ophalen van de posts probeer het opniew! " + e)
+                console.error("Er ging iets mis met het ophalen van de posts probeer het opniew! " + e)
             }
         }
-
         fetchPatterns();
 
     }, []);
 
-    useEffect(() => {
+
+    async function fetchComments() {
         const token = localStorage.getItem('token');
-        async function fetchComments() {
-            try {
-                const result = await axios.get(`http://localhost:8080/posts/${id}/comments`,
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: token,
-                        }
-                    })
-                setComments(result.data);
-                console.log(result.data);
-            } catch (e) {
-                console.log("Er ging iets mis met het ophalen van de posts probeer het opniew! " + e)
-            }
+        try {
+            const result = await axios.get(`http://localhost:8080/posts/${id}/comments`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: token,
+                    }
+                })
+            setComments(result.data);
+        } catch (e) {
+            console.error("Er ging iets mis met het ophalen van de posts probeer het opniew! " + e)
         }
-
-        fetchComments();
-    }, []);
-
+    }
+    fetchComments();
 
     return <main>
         <section className="post-detail-section">
             {postDetail.image && <article className="post-big">
                 <span className="post-img">
                     <img src={postDetail.image.url} alt={postDetail.image.fileName}/>
-                    <Button classname="icon-button close-post" type="button" img={closeIcon} alt="Sluit icoon"/>
+                    <Button classname="icon-button close-post" type="button" img={closeIcon} alt="Sluit icoon" onClick={(() => navigate("/"))}/>
                 </span>
                 <div className="details-comments">
                     <div className="post-details">
@@ -120,35 +115,24 @@ function PostDetails() {
                         <h2>{postDetail.title}</h2>
                         <p>{postDetail.description}</p>
                     </div>
-                    <div className="comment-section">
+                    <div className="comment-section"> <div>
                         {comments.length ? comments.map((comment) => (
-                            <article className="comment" key={comment.id}>
-                                <img src={userIcon} alt="profiel foto"/>
-                                <div className="comment-text">
-                                    <h5>@{comment.username}</h5>
-                                    <p className="message">{comment.message}</p>
-                                    <div className="comment-details">
-                                        <p>{comment.timeStamp}</p> <p>reageren</p> <p>{comment.likes}</p><Button
-                                        classname="like-button" img={heartOutline} alt="like icoon" type="button"/>
-                                    </div>
-                                    <p className="sub-comments">lijst andere comments <img src={arrowDownBlue}
-                                                                                           alt="pijl icoon"/></p>
-                                </div>
-                            </article>
+                            <Comment key={comment.id} img={userIcon} alt="profiel foto" username={comment.username} message={comment.message} timeStamp={comment.timeStamp} commentLikes={comment.likes}  />
                         )) : <p>Schrijf een reactie voor deze post!</p>}
                         {comments.length > 0 && <h5>22 reacties <img src={arrowDownBlue} alt="pijl icoon"/></h5>}
+                    </div>
                         <div className="interaction-part">
                             <form className="comment-form" onSubmit={handleSubmit(handleFormSubmit)}>
                                 <Input className="text-field-red" inputId="comment" name="comment" type="text"
                                        placeholder="Reageren..." register={register} errors={errors}/>
                                 <Button classname="comment-button blue interact-button" type="submit"
-                                        img={arrowDownWhite} alt="pijl icoon"/>
+                                        img={arrowDownWhite} alt="pijl icoon" />
                             </form>
 
                             <Button classname="interact-button like-button-big red" type="button"
                                     text={postDetail.likes} img={heart}
                                     alt="hartjes icoon"/>
-                            <Button classname="interact-button" type="button" img={addIcon} alt="plus icoon"/>
+                            <Button classname="interact-button" type="button" img={addIcon} onClick={(() => navigate("/new-pattern"))} alt="plus icoon"/>
 
                         </div>
 
